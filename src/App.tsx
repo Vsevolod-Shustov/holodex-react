74716aa9231adade0219d70efd31d05c15ae7d5a
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2023-10-17 10:26:08 +0300

    api key in .env.local, fetch to test it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
 import './App.css'
 
-function App() {
+const HOLODEX_API_KEY = import.meta.env.VITE_HOLODEX_API_KEY
 
+async function getLive() {
+  const response = await fetch("https://holodex.net/api/v2/videos?status=live&max_upcoming_hours=24&org=Hololive", {
+    headers: {
+      "X-APIKEY": HOLODEX_API_KEY
+    }
+  });
+  const live = await response.json();
+  console.log(live);
+}
+
+function App() {
+  useEffect(() => {
+    getLive()
+  },[])
   return (
     <>
       app
