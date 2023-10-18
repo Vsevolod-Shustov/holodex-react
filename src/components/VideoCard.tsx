7c81d5cf11e25b2d18e02833f655ba7309271e68
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2023-10-18 17:22:33 +0300

    tailwind, grid, channel thumbnails

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,8 +1,21 @@
 export default function VideoCard(props: any) {
 
     return(
-        <div>
-            <img src={`http://img.youtube.com/vi/${props.video.id}/maxresdefault.jpg`}></img>
+        <div className="relative">
+            <a
+            href={`https://youtu.be/${props.video.id}`}
+            target="_blank"    
+            >
+                <img
+                className="rounded"
+                src={`http://img.youtube.com/vi/${props.video.id}/maxresdefault.jpg`}
+                ></img>
+                <img
+                className="absolute top-2 left-2 rounded-full"
+                src={`https://holodex.net/statics/channelImg/${props.video.channel.id}/50.png`}
+                ></img>
+            </a>
+            
         </div>
     )
 }
--- src/components/VideoGrid.tsx
+++ src/components/VideoGrid.tsx
@@ -47,8 +47,8 @@ export default function VideoGrid() {
     useEffect(() => {}, [])
 
     return (
-    <>
+      <div className="grid gap-1 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {videos?.map((video) => <VideoCard key={video.id} video={video}></VideoCard>)}
-    </>
+      </div>
     )
 }
