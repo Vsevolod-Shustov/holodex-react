cead92725592a35c8802b71b5b004b5b12ce66ed
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2023-10-23 18:10:33 +0300

    card layout, grid gap

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,22 +1,53 @@
 import { useMemo } from "react"
 
 export default function VideoCard(props: any) {
-    const channelImg = useMemo(() => props.video.channel.photo.replace("=s800", "=s50"), [props.video.channel.photo])
+    const channelImg = useMemo(() => props.video.channel.photo.replace("=s800", "=s48"), [props.video.channel.photo])
 
     return(
-        <div className="relative">
+        <div
+        className="video-card relative">
             <a
+            className="block"
             href={`https://youtu.be/${props.video.id}`}
-            target="_blank"    
+            target="_blank"
             >
-                <img
-                className="rounded"
-                src={`http://img.youtube.com/vi/${props.video.id}/maxresdefault.jpg`}
-                ></img>
-                <img
-                className="absolute top-2 left-2 rounded-full"
-                src={channelImg}
-                ></img>
+                <div
+                className="video-thumbnail relative">
+                    <img
+                    className="rounded"
+                    src={`http://img.youtube.com/vi/${props.video.id}/maxresdefault.jpg`}
+                    ></img>
+                    <div
+                    className="video-topic absolute top-1 left-1 rounded-sm bg-black bg-opacity-75 text-white text-sm px-1 py-px">
+                        {props.video.topic_id}
+                    </div>
+                </div>
+
+                <a
+                className="video-text flex"
+                href={`https://youtu.be/${props.video.id}`}
+                target="_blank">
+                    <div className="flex-none p-2">
+                        <img
+                        className="rounded-full w-12 h-12"
+                        src={channelImg}
+                        ></img>
+                    </div>
+                    <div>
+                        <div>
+                            {props.video.title}
+                        </div>
+                        <div>
+                            <a
+                            className="text-blue-400"
+                            target="_blank"
+                            href={`https://www.youtube.com/channel/${props.video.channel.id}`}>
+                                {props.video.channel.english_name}
+                            </a>
+                        </div>
+                    </div>
+                    
+                </a>
             </a>
             
         </div>
--- src/components/VideoGrid.tsx
+++ src/components/VideoGrid.tsx
@@ -47,7 +47,7 @@ export default function VideoGrid() {
     useEffect(() => {}, [])
 
     return (
-      <div className="grid gap-1 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {videos?.map((video) => <VideoCard key={video.id} video={video}></VideoCard>)}
       </div>
     )
