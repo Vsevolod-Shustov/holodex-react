b9ca0fbbd301e7ed21ba87adaf5e06917f6d6152
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2023-10-30 17:03:30 +0300

    fixed link nesting in VideoCard

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -6,27 +6,26 @@ export default function VideoCard(props: any) {
     return(
         <div
         className="video-card relative">
-            <a
-            className="block"
-            href={`https://youtu.be/${props.video.id}`}
-            target="_blank"
-            >
+            <div>
                 <div
                 className="video-thumbnail relative">
-                    <img
-                    className="rounded"
-                    src={`http://img.youtube.com/vi/${props.video.id}/maxresdefault.jpg`}
-                    ></img>
-                    <div
-                    className="video-topic absolute top-1 left-1 rounded-sm bg-black bg-opacity-75 text-white text-sm px-1 py-px">
-                        {props.video.topic_id}
-                    </div>
+                    <a
+                    className="block"
+                    href={`https://youtu.be/${props.video.id}`}
+                    target="_blank"
+                    >
+                        <img
+                        className="rounded"
+                        src={`http://img.youtube.com/vi/${props.video.id}/maxresdefault.jpg`}
+                        ></img>
+                        <div
+                        className="video-topic absolute top-1 left-1 rounded-sm bg-black bg-opacity-75 text-white text-sm px-1 py-px">
+                            {props.video.topic_id}
+                        </div>
+                    </a>
                 </div>
 
-                <a
-                className="video-text flex"
-                href={`https://youtu.be/${props.video.id}`}
-                target="_blank">
+                <div className="video-text flex">
                     <div className="flex-none p-2">
                         <img
                         className="rounded-full w-12 h-12"
@@ -35,7 +34,12 @@ export default function VideoCard(props: any) {
                     </div>
                     <div>
                         <div>
-                            {props.video.title}
+                            <a
+                            className="video-text flex"
+                            href={`https://youtu.be/${props.video.id}`}
+                            target="_blank">
+                                {props.video.title}
+                            </a>
                         </div>
                         <div>
                             <a
@@ -46,9 +50,8 @@ export default function VideoCard(props: any) {
                             </a>
                         </div>
                     </div>
-                    
-                </a>
-            </a>
+                </div>
+            </div>
             
         </div>
     )
