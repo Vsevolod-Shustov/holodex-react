eac053c52367e485ca1c055e96aafa72df31923a
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2023-10-30 19:02:45 +0300

    card live/upcoming time, title truncate

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,7 +1,29 @@
-import { useMemo } from "react"
+import { useEffect, useMemo, useState } from "react"
 
 export default function VideoCard(props: any) {
     const channelImg = useMemo(() => props.video.channel.photo.replace("=s800", "=s48"), [props.video.channel.photo])
+
+    const [liveDuration, setLiveDuration] = useState("")
+    const [startsIn, setStartsIn] = useState("")
+
+    useEffect(() => {
+        if(props.video.status === "live") {
+            const start_actual = Date.parse(props.video.start_actual)
+            const now = Date.now()
+            const live_duration = now - start_actual
+            const dateObj = new Date(live_duration)
+            const duration_string = dateObj.toISOString().substring(11, 19)
+            setLiveDuration(duration_string)
+        } else if(props.video.status === "upcoming") {
+            const start_scheduled = Date.parse(props.video.start_scheduled)
+            const now = Date.now()
+            const starts_in = start_scheduled - now
+            const dateObj = new Date(starts_in)
+            const starts_in_string = dateObj.toISOString().substring(11, 19)
+            setStartsIn(starts_in_string)
+        }
+    }, [])
+    
 
     return(
         <div
@@ -24,12 +46,12 @@ export default function VideoCard(props: any) {
                         </div>
                         
                             {props.video.status === "live" ? <div
-                        className="video-live absolute bottom-1 right-1 rounded-sm bg-red-800 bg-opacity-80 text-white text-sm px-1 py-px">Live</div> : ""}
+                        className="video-live absolute bottom-1 right-1 rounded-sm bg-red-800 bg-opacity-80 text-white text-sm px-1 py-px">{liveDuration}</div> : ""}
                         
                     </a>
                 </div>
 
-                <div className="video-text flex">
+                <div className="video-text flex mt-2">
                     <div className="flex-none p-2">
                         <img
                         className="rounded-full w-12 h-12"
@@ -37,13 +59,8 @@ export default function VideoCard(props: any) {
                         ></img>
                     </div>
                     <div>
-                        <div>
-                            <a
-                            className="video-text flex"
-                            href={`https://youtu.be/${props.video.id}`}
-                            target="_blank">
-                                {props.video.title}
-                            </a>
+                        <div className="max-h-10 leading-5 line-clamp-2">
+                            {props.video.title}
                         </div>
                         <div>
                             <a
@@ -52,6 +69,11 @@ export default function VideoCard(props: any) {
                             href={`https://www.youtube.com/channel/${props.video.channel.id}`}>
                                 {props.video.channel.english_name}
                             </a>
+                        </div>
+                        <div>
+                        {props.video.status === "live" ?
+                        <span><span className="text-red-500">Live Now</span> • {props.video.live_viewers} watching</span> : 
+                        <span>Starts in {startsIn}</span>}
                         </div>
                     </div>
                 </div>
