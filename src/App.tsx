d289c95feecc81f0736f84f3b7025c1a879c7f29
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2023-11-01 16:35:44 +0300

    header component, app 'layout'

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css'
+import Header from './components/Header';
 import Home from './components/Home';
 
-
-
 function App() {
-  
   return (
-    <>
-      <Home></Home>
-    </>
+    <div className='app dark'>
+      <div className='styles-wrapper dark:bg-slate-900 dark:text-slate-100'>
+        <Header></Header>
+        <div className='content p-4 '>
+          <Home></Home>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  
+  return (
+    <div className="px-4 py-1 bg-sky-500">
+        here be header
+    </div>
+  )
+}
+
+export default Header
