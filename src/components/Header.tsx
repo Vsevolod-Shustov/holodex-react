a5e30ab795161169bfb48d8fa4ae5d141808ced8
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2023-11-06 16:06:01 +0300

    dropdown styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,23 @@
 import Dropdown from "./ui/Dropdown"
 
+const items = [
+  { title: "Item 1" },
+  { title: "Item 2" },
+  { title: "Item 3" },
+  { title: "Item 4" },
+  { title: "a" },
+  { title: "Lorem ipsum dolor sit amet" },
+]
+
 function Header() {
 
   return (
     <div className="Header flex px-4 py-1 bg-sky-500">
       here be header
       <Dropdown>
-        <div className="w-200 bg-sky-500">
+        <div className="rounded bg-slate-900 py-1 w-max">
           <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-            <li>Item 4</li>
+            {items.map((item) => (<li className="hover:bg-slate-500 py-1 px-4 ">{item.title}</li>))}
           </ul>
         </div>
       </Dropdown>
