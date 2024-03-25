90f9d06c3c11dbd03a208a2719ff774a6276dd64
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2024-03-25 15:02:17 +0300

    video grid padding

--- src/components/VideoGrid.tsx
+++ src/components/VideoGrid.tsx
@@ -6,7 +6,7 @@ import { liveData } from "./Home";
 type Video = {
   id: string,
   title: string,
-  type:string,
+  type: string,
   topic_id: string,
   published_at: string,
   available_at: string,
@@ -16,14 +16,14 @@ type Video = {
 }
 
 export default function VideoGrid() {
-    const [videos] = useAtom(liveData)
+  const [videos] = useAtom(liveData)
 
-    return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {videos?.map((video: Video) => <VideoCard key={video.id} video={video}></VideoCard>)}
-          test
-        </div>
-      </Suspense>
-    )
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="p-4 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {videos?.map((video: Video) => <VideoCard key={video.id} video={video}></VideoCard>)}
+        test
+      </div>
+    </Suspense>
+  )
 }
