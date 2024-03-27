9ebf6c7ddbe7fa5fee14c5b31b79e94458f9bcb3
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2024-03-27 09:04:38 +0300

    add basename to router

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,8 @@ const router = createBrowserRouter([
     ],
   },
 
-]);
+],
+  { basename: "/holodex-react/" });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
