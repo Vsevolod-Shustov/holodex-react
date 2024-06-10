91d2e7065d7ee4e69ce787326b4b3ce34ae63c89
Vsevolod Shustov <vsevolod.shustov@gmail.com>
2024-06-10 16:56:26 +0300

    remove 'test' line from VideoGrid

--- src/components/VideoGrid.tsx
+++ src/components/VideoGrid.tsx
@@ -16,7 +16,6 @@ export default function VideoGrid() {
   return (
     <div className="p-4 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {liveData?.map((video: Video) => <VideoCard key={video.id} video={video}></VideoCard>)}
-      test
     </div>
   )
 }
